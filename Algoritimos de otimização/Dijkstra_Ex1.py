0d6c2dfa74897b455a462d8052ed7686c5721407
LucasAlves2803 <alveshugo2906@gmail.com>
2024-03-18 19:11:06 -0300

    adicionando algoritmo

---
+++ Algoritimos de otimização/Dijkstra_Ex1.py
@@ -0,0 +1,8 @@
+# O algoritmo de Dijkstra é um método para encontrar o caminho mais curto entre dois pontos em um grafo. Aqui está uma explicação passo a passo:
+
+# Inicialização: Começamos selecionando um nó inicial. Definimos a distância deste nó para si mesmo como 0 e a distância para todos os outros nós como infinito.
+# Visitação: Visitamos todos os vizinhos do nó inicial e atualizamos suas distâncias. A distância de um nó é a soma da distância do nó inicial até o nó atual e o peso da aresta que conecta o nó atual ao seu vizinho.
+# Atualização: Depois de visitar todos os vizinhos do nó inicial, escolhemos o nó com a menor distância que ainda não foi visitado. Este nó se torna nosso novo “nó atual”.
+# Repetição: Repetimos os passos 2 e 3 até que tenhamos visitado todos os nós ou encontrado o nó de destino.
+# Resultado: O resultado é o caminho mais curto do nó inicial ao nó de destino.
+# Aqui está um exemplo visual de como o algoritmo funciona:
