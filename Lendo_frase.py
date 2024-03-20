6b21c0c8a2e1137b21ef19a65c0740b18a2dc83d
LucasAlves2803 <alveshugo2906@gmail.com>
2024-03-20 18:34:34 -0300

    ok

--- Lendo_frase.py
+++ Lendo_frase.py
@@ -1,5 +1,6 @@
 
 frase = "flamengo é o melhor"
+print(frase)
 palavra = 'campeao'
 
 frase_lista = frase.split()
@@ -12,3 +13,20 @@ ultima_palavra_depois = palavra
 print("antes",ultima_palavra_antes)
 print("depois",ultima_palavra_depois)
 
+frase_lista[-1] = ultima_palavra_depois
+print(frase_lista)
+
+frase_final = " ".join(frase_lista)
+print(frase_final)
+
+def ultima_palavra(frase,palavra):
+    print(frase)
+    frase_lista = frase.split()
+    ultima_palavra_antes = frase_lista[-1]
+    ultima_palavra_depois = palavra
+    frase_lista[-1] = ultima_palavra_depois
+    frase_final = " ".join(frase_lista)
+
+    return frase_final
+
+print(ultima_palavra("flamengo é o melhor", "pior"))
