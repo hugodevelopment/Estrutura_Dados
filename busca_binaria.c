8cdf584516243cfa6d3c191e854ed8c66ea6d1ec
LucasAlves2803 <alveshugo2906@gmail.com>
2024-03-25 15:35:33 -0300

    ok

---
+++ busca_binaria.c
@@ -0,0 +1,19 @@
+//Implementação Iterativa:
+
+int PesquisaBinaria (int vet[], int chave, int Tam)
+{
+     int inf = 0;     // limite inferior (o primeiro índice de vetor em C é zero          )
+     int sup = Tam-1; // limite superior (termina em um número a menos. 0 a 9 são 10 números)
+     int meio;
+     while (inf <= sup)
+     {
+          meio = (inf + sup)/2;
+          if (chave == vet[meio])
+               return meio;
+          if (chave < vet[meio])
+               sup = meio-1;
+          else
+               inf = meio+1;
+     }
+     return -1;   // não encontrado
+}
